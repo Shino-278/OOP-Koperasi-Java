2b1715f2162ae6a90a1b37e7dfc96ee04fd17648
Shino <116130450+Shino-278@users.noreply.github.com>
2022-10-19 13:16:00 +0700

    Add files via upload

---
+++ Koperasi.java
@@ -0,0 +1,53 @@
+
+class koperasi {
+    
+    String idAnggota;
+    String namaAnggota;
+    String nomorRekening;
+    
+    int pinjam;
+    double saldo,pajak;
+    
+    //method non void
+    
+    public koperasi (String idAnggota, String namaAnggota, String nomorRekening){
+        this.idAnggota = idAnggota;
+        this.namaAnggota = namaAnggota;
+        this.nomorRekening = nomorRekening;
+    }
+   
+    koperasi(int saldo, int pinjam, double pajak){
+        this.saldo = saldo;
+        this.pinjam = pinjam;
+        this.pajak = pajak;
+    }
+    
+    double hitungSaldo (){
+        return this.saldo = (saldo + (pinjam - (pinjam * pajak)));
+    }
+    
+    //Method Void
+    
+    void tampil (){
+        System.out.println ("Id Anggota : "+this.idAnggota);
+        System.out.println ("Nama Anggota : "+this.namaAnggota);
+        System.out.println ("Nomor Rekenng : "+this.nomorRekening);
+        System.out.println ();
+    }
+    
+    void ambilSaldo (){
+        System.out.println ("Saldo anda : "+ saldo);
+        System.out.println ("Jumlah pinjaman : "+this.pinjam);
+        System.out.println ("Bunga pinjaman : "+this.pajak);
+        System.out.println ("Sisa saldo anda sebesar : "+ this.hitungSaldo());
+    }
+}
+
+public class Koperasi {
+    public static void main ( String [] args){
+        koperasi anggota = new koperasi ("ID-01","Muhammad Bagir","RK-007");
+        koperasi ambil = new koperasi (100000,50000,0.10);
+        anggota.tampil();
+        ambil.ambilSaldo();
+    }
+}
